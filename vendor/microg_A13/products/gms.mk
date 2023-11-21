c5f8e156408e6aaa7a0d88a2cb1c3d7bc76c162b
PizzaG <49663808+PizzaG@users.noreply.github.com>
2023-11-21 03:48:46 -0600

    Delete vendor/microg_A13 directory

--- vendor/microg_A13/products/gms.mk
+++
@@ -1,9 +0,0 @@
-PRODUCT_PACKAGES += \
-	GmsCore \
-	GsfProxy \
-	FakeStore \
-	IchnaeaNlpBackend \
-	NominatimGeocoderBackend \
-	FDroid \
-	FDroidPrivilegedExtension \
-	additional_repos.xml
--- vendor/microg_A13/products/gms_64bit_only.mk
+++
@@ -1,1 +0,0 @@
--include $(LOCAL_PATH)/gms.mk

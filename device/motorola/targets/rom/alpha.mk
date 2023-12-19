ac743d5a5931f3ab57d81e6186e0a26984730bc0
PizzaG <pizzag98@gmail.com>
2023-12-18 20:37:16 -0600

    update alpha.mk with proper gapps build command

--- device/motorola/targets/rom/alpha.mk
+++ device/motorola/targets/rom/alpha.mk
@@ -6,27 +6,25 @@
 # Common
 include device/motorola/targets/include/common.mk
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Kernel
 $(call inherit-product, device/motorola/targets/include/kernel/source.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
-#$(call inherit-product, vendor/v4afx/config.mk)
 
 # AlphaDroid
 ALPHA_BUILD_TYPE := UNOFFICIAL
 ALPHA_MAINTAINER := PizzaG
-WITH_GAPPS := true
+WITH_GMS := true
 EXTRA_UDFPS_ANIMATIONS := false
 TARGET_HAS_UDFPS := false
 TARGET_ENABLE_BLUR := false
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_CALL_RECORDING := true
-
 
 # Required Scripts
 $(warning This ROM requires replace_camera_sepolicy.sh to be ran...)

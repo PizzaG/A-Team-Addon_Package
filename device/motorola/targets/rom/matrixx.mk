41d9c933d0f06cdf45a74fe99b629960ff952e26
PizzaG <pizzag98@gmail.com>
2023-12-22 05:24:12 -0600

    add some device specific info

--- device/motorola/targets/rom/matrixx.mk
+++ device/motorola/targets/rom/matrixx.mk
@@ -16,9 +16,29 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 MATRIXX_BUILD_TYPE := Unofficial
 MATRIXX_MAINTAINER := PizzaG
 WITH_GAPPS := true
-#MATRIXX_CHIPSET := SM8350
-#MATRIXX_BATTERY := 4500mAh
-#MATRIXX_DISPLAY := 1080x2400
+
+# Auto Matrixx Device Selection
+ifeq ($(DEVICE_NAME),Amogus)
+    MATRIXX_CHIPSET := SM6125
+    MATRIXX_BATTERY := 4000mAh
+    MATRIXX_DISPLAY := 1080x2300
+else ifeq ($(DEVICE_NAME),Borneo)
+    MATRIXX_CHIPSET := SM6115
+    MATRIXX_BATTERY := 5000mAh
+    MATRIXX_DISPLAY := 1080x2400
+else ifeq ($(DEVICE_NAME),Guamna)
+    MATRIXX_CHIPSET := SM4250
+    MATRIXX_BATTERY := 5000mAh
+    MATRIXX_DISPLAY := 1080x2400
+else ifeq ($(DEVICE_NAME),Milanf)
+    MATRIXX_CHIPSET := SM6375
+    MATRIXX_BATTERY := 5000mAh
+    MATRIXX_DISPLAY := 1080x2460
+else ifeq ($(DEVICE_NAME),Osaka)
+    MATRIXX_CHIPSET := SM4350
+    MATRIXX_BATTERY := 5000mAh
+    MATRIXX_DISPLAY := 1080x2400
+endif
 
 # Required Scripts
 $(warning This ROM requires replace_camera_sepolicy.sh to be ran...)

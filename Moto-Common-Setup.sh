fd728738066b924cb3741b91354d7bf00b22ab93
PizzaG <pizzag98@gmail.com>
2023-12-22 22:31:53 -0600

    add seperate bot for each device, add setup for copying bot folder

--- Moto-Common-Setup.sh
+++ Moto-Common-Setup.sh
@@ -25,8 +25,8 @@ echo ""
 echo "A-Team Moto-Common Setup Script"
 echo ""
 echo ""
-echo "Setup Version: 0.10"
-echo "Setup Update: 12-21-2023"
+echo "Setup Version: 0.11"
+echo "Setup Update: 12-22-2023"
 echo ""
 echo ""
 echo "Current Date: $date"
@@ -173,6 +173,13 @@ echo ""
 echo "1/1..."
 echo ""
 cp -r $A_TEAM_PACKAGE_LOCATION/device/motorola/A-Team device/motorola
+sleep 5
+echo "Copying A-Team Telegram Bot Folder To Rom Root..."
+echo ""
+echo "1/1..."
+echo ""
+mkdir Telegram_Bots
+cp -r $A_TEAM_PACKAGE_LOCATION/Telegram_Bots/* Telegram_Bots
 sleep 5
 ## Patch CommonConfig.mk
 echo "Patching CommonConfig.mk..."
---
+++ Telegram_Bots/Telegram_Bot-Borneo.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#
+# Copyright 2019-Present A-Team Digital Solutions
+#
+# A-Team Moto-Common Telegram Notification Bot Addon
+#
+# Bot Version: 0.03"
+# Bot Update Date: 12-22-2023"
+#
+
+
+# Telegram Bot Variables - **User Adaptable**
+export CHANNEL_ID='-1001881525724'
+export BOT_TOKEN='6980538384:AAFwUFeMM1ML_Jal5N_d7-EDU793WMykgx4'
+
+
+# Device Specific Thread Topic Variables - **User Adaptable**
+export BORNEO_THREAD_ID='17716'
+
+
+# if the first argument is "-h" for help.
+# display the usage information
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` \"text message\""
+  exit 0
+fi
+
+# Warn if the first argument is empty.
+if [ -z "$1" ]; then
+    echo "Add message text as the second argument"
+    exit 0
+fi
+
+# Warn if more than one argument is passed to the script.
+if [ "$#" -ne 1 ]; then
+    echo "You can pass only one argument. For string with spaces, put it on quotes"
+    exit 0
+fi
+
+# send a POST request to Telegram's API
+# The '-s' = "silent" mode
+# redirect outputs to /dev/null
+curl -s --data "text=$1" --data "chat_id=$CHANNEL_ID" --data "message_thread_id=$BORNEO_THREAD_ID" 'https://api.telegram.org/bot'$BOT_TOKEN'/sendMessage' > /dev/null. follow instructions to create bot & get token to access the HTTP API.

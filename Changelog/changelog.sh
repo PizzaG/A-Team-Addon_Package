ee10860f96418f6d64ef9c20568444dcbcb5e661
PizzaG <pizzag98@gmail.com>
2024-07-01 23:25:12 -0500

    update changelog stuffs

--- Changelog/changelog.sh
+++ Changelog/changelog.sh
@@ -4,20 +4,8 @@
 # Copyright 2019-Present A-Team Digital Solutions
 #
 
-# Exports
-out=$OUT_DIR/target/product/$DEVICE_CODENAME
-if [ -f $out ]; # The path does not exist
-then
-	echo "The path to create changelog does not exist, exiting..."
-	exit
-fi
-
-export Changelog=Changelog.txt
-
-touch $Changelog
-
 # Print something to build output
+echo ""
 echo "Generating A-Team Custom Changelog..."
+echo ""
 
-cp $Changelog $OUT_DIR/target/product/$DEVICE_CODENAME/system/etc/$Changelog
-cp $Changelog $OUT_DIR/target/product/$DEVICE_CODENAME/

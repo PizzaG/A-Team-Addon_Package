bd9972bbda8960f871dadb8abe04cbf3d2a9fa5d
PizzaG <pizzag98@gmail.com>
2024-07-12 21:27:38 -0500

    remove clear so user will see all patching in case of error

--- device/motorola/A-Team/Final_Zip/AIK/KSU/PBRP-R/repackimg.sh
+++ device/motorola/A-Team/Final_Zip/AIK/KSU/PBRP-R/repackimg.sh
@@ -89,7 +89,7 @@ if [ -z "$(ls ramdisk/* 2>/dev/null)" ] && [ ! "$ramdiskcomp" = "empty" -a ! "$o
   exit 1;
 fi;
 
-clear;
+echo " ";
 echo " ";
 echo "Android Image Kitchen - RepackImg Script";
 echo "by osm0sis @ xda-developers";

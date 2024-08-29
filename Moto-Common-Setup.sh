1c44d07983536c94054701b8720b1613d2eab337
PizzaG <pizzag98@gmail.com>
2024-08-28 21:06:03 -0500

    updates and add alt patch

--- Moto-Common-Setup.sh
+++ Moto-Common-Setup.sh
@@ -7,8 +7,8 @@
 #
 
 APP_NAME="A-Team Moto-Common Setup Script" 
-SETUP_VERSION="0.19"
-SETUP_DATE="7-13-2024"
+SETUP_VERSION="0.20"
+SETUP_DATE="8-29-2024"
 
 # Date
 date=$(date -u +%-m/%d/%Y)
@@ -309,6 +309,15 @@ else
       rm -rf frameworks/base/core/java/android/os/Build.java.rej
       mv frameworks/base/core/java/android/os/Build.java.orig frameworks/base/core/java/android/os/Build.java
       patch -u -b frameworks/base/core/java/android/os/Build.java -i device/$DT_MANUFACTURER/A-Team/Build.java-alt.patch
+      echo ""
+   fi
+   if [ -f frameworks/base/core/java/android/os/Build.java.rej ]; then
+      echo "Patch Failed, Trying Alternate 1 Patch File..."
+      echo ""
+      rm -rf frameworks/base/core/java/android/os/Build.java
+      rm -rf frameworks/base/core/java/android/os/Build.java.rej
+      mv frameworks/base/core/java/android/os/Build.java.orig frameworks/base/core/java/android/os/Build.java
+      patch -u -b frameworks/base/core/java/android/os/Build.java -i device/$DT_MANUFACTURER/A-Team/Build.java-alt-1.patch
       echo ""
    fi
    if [ -f frameworks/base/core/java/android/os/Build.java.rej ]; then
--- ROM_MK_FILES/afterlife.mk
+++ ROM_MK_FILES/afterlife.mk
@@ -13,7 +13,6 @@ $(call inherit-product, device/motorola/targets/include/kernel/source.mk)
 $(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # AfterLife stuff
-AFTERLIFE_GAPPS := true
 AFTERLIFE_MAINTAINER := 
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_BLUR := true
@@ -22,6 +21,19 @@ USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_SUPPORTS_QUICK_TAP := true
+
+# Gapps Selection
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+ifeq ($(ROM_BUILD_TYPE),GAPPS)
+  AFTERLIFE_GAPPS := true
+#else
+#  ifeq ($(ROM_BUILD_TYPE),MICROG)
+#    TARGET_BUILD_PACKAGE := 2
+#  endif
+endif
 
 # Required Scripts
 $(warning This ROM requires replace_camera_sepolicy.sh to be ran...)
